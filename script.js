b6ce96943cd7acb6a7bc3de644aed57e54060a6e
Gayathri-SR <gayathrisr855@gmail.com>
2026-08-31 20:54:30 +0530

    Handling empty tasks, clearing input after task addition and adding eventlistener to the checkboxes (Event bubbling)

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const addTaskButton = document.querySelector('.add-task');
+const taskList = document.querySelector('.task-list');
 addTaskButton.addEventListener("click", function() {
+    const taskInput = document.querySelector('.task-input');
+    const task = taskInput.value.trim();
+
+    if(task === "") {
+        alert("Please enter a valid text"); //To be made UI friendly later
+        return;
+    }
     const emptyText = document.querySelector('.empty-list');
     if(!emptyText.classList.contains('hidden')) emptyText.classList.add('hidden');
 
-    const taskList = document.querySelector('.task-list');
-    const task = document.querySelector('.task-input').value;
     const taskItem = `<div class="task-item"><input type="checkbox" />${task}</div>`;
-
     taskList.innerHTML += taskItem;
+    taskInput.value = '';
+});
+
+taskList.addEventListener("click", function(e) {
+    if(e.target.type === 'checkbox') {
+        console.log("Checkbox clicked");
+    }
 });
